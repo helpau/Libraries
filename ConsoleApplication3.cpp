f3ed1d0aa355cb996168ef1ed4789391d0ee5d0b
Adam Smith <maxmakskanal@outlook.com>
2022-06-17 18:34:01 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "MyCoolLib_lol.cpp"
+using namespace std;
+
+int triple(int a, int b) {
+    return 3 * add(a, b);
+}
+int main()
+{
+    cout<<add(3, 2)<<endl;
+    cout << triple(3, 2)<<endl;
+}
---
+++ MyCoolLib_lol.cpp
@@ -0,0 +1,4 @@
+//#include "MyCoolLib.h"
+int add(int a, int b) {
+    return a + b;
+}
