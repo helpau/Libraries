d1f5584a9ff5f1f4c5a2d777e0847ecf9cd9792d
Adam Smith <maxmakskanal@outlook.com>
2022-06-17 20:03:34 +0500

    DLL added

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -1,14 +1,19 @@
 ﻿#include <iostream>
 //#include "MyCoolLib.h"
-#include "..\StaticLib1\StaticLib1.h"
+//#include "..\StaticLib1\StaticLib1.h"
+#include <Windows.h>
 using namespace std;
-
+/*
 int triple(int a, int b) {
     return 3 * add(a, b);
-}
+}*/
 int main()
 {
-
+    HINSTANCE LOAD;
+    auto load = LoadLibrary(L"Dll1.dll");
+    typedef int(*add_dll)(int, int);
+    add_dll add;
+    add = (add_dll)GetProcAddress(load, "add");
     cout<<add(3, 2)<<endl;
-    cout << triple(3, 2)<<endl;
+    //cout << triple(3, 2)<<endl;
 }
